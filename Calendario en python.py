ee9fcb7dafd0f0cddc8f9f4ae8d0d1a65c9f789f
Miranda_bj_Ajax <brayanjosu789@gmail.com>
2024-10-08 08:54:56 -0600

    Add files via upload

---
+++ Calendario en python.py
@@ -0,0 +1,11 @@
+# CALENDARIO EN PYTHON
+import calendar
+
+
+yy = int(input("Introduce un año: "))
+mm = int(input("Introduce un mes: "))
+
+print('  Python Calendar\n ')
+
+print(calendar.month(yy,mm))
+
